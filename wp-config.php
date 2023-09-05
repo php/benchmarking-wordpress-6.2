ef263dad5e1e6bbc78885cb6707c0a4d07ad5fc6
Ilija Tovilo <ilija.tovilo@me.com>
2023-09-05 17:24:57 +0200

    Disable cronjobs
    Hopefully that makes profiling more consistent.

--- wp-config.php
+++ wp-config.php
@@ -83,7 +83,7 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-
+define('DISABLE_WP_CRON', true);
 
 /* That's all, stop editing! Happy publishing. */
 
